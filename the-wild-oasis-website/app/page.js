7329a15649f5977c5d875c3e63af5f1b8cdc360b
Maciej Kawęcki <skimiskim12@gmail.com>
2024-05-27 19:25:33 +0200

    React server componets understing, starter pack

---
+++ the-wild-oasis-website/app/page.js
@@ -0,0 +1,9 @@
+import Navigation from "./components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+    </div>
+  );
+}
