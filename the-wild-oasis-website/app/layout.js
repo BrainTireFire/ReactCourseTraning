ebbbc9bdfa09902bd493f48f04e44145a9513518
Maciej Kawęcki <skimiskim12@gmail.com>
2024-05-31 18:00:21 +0200

    Added the pages, navigations, images handling, styles, links, start with project

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -1,20 +1,34 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Header from "@/app/_components/Header";
+
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  display: "swap",
+  subsets: ["latin"],
+  //weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurioous cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
