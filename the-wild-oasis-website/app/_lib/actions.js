73df7173fcf83061f7f9fc6e18b695feac374867
Maciej Kawęcki <skimiskim12@gmail.com>
2024-08-31 16:04:00 +0200

    server action, update guest data, revaldiate and loading indicator

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -1,6 +1,33 @@
 "use server";
 
-import { signIn, signOut } from "@/app/_lib/auth";
+import { auth, signIn, signOut } from "@/app/_lib/auth";
+import { updateGuest } from "./data-service";
+import { revalidatePath } from "next/cache";
+
+export async function updateGuestAction(formData) {
+  const session = await auth();
+
+  if (!session) {
+    throw new Error("You must be logged in");
+  }
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error("Please provide a valid national ID number");
+  }
+
+  const updateData = {
+    nationality,
+    nationalID,
+    countryFlag,
+  };
+
+  await updateGuest(session.user.guestId, updateData);
+
+  revalidatePath("/account/profile");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
