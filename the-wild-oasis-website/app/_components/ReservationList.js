23d049b41da707e323b52572ea10c1b2d917c466
Maciej Kawęcki <skimiskim12@gmail.com>
2024-09-02 15:59:03 +0200

    Removing immediately, useOptimistic

---
+++ the-wild-oasis-website/app/_components/ReservationList.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { deleteReservationAction } from "../_lib/actions";
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (currentBookings, bookingId) => {
+      return currentBookings.filter((booking) => booking.id !== bookingId);
+
+      // if you want to add a new booking, you can do it like this:
+      // [...currentBookings, newBooking];
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservationAction(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+          onDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
